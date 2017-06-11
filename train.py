11160054b006e375b31e02b0f03ad8b7de57e700
Valery Piashchynski <piashchynski_valery@hotmail.com>
2017-06-11 16:40:30 +0300

    Some fixes

--- train.py
+++ train.py
@@ -52,6 +52,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test),
           callbacks=[tbCallBack])
 
+
 # plot_model(model, 'model.png')
 
 score = model.evaluate(x_test, y_test)
